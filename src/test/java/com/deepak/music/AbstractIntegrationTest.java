36d10cdf0d261009c5e1b43af1e9be7c4aaee5de
Deepak <connect2dpk@gmail.com>
2026-07-18 11:34:01 +0100

    Spring boot project setup

---
+++ src/test/java/com/deepak/music/AbstractIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.deepak.music;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+@Testcontainers
+public abstract class AbstractIntegrationTest {
+
+    @Container
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18-alpine")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void configure(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+}
+
---
+++ src/test/java/com/deepak/music/MusicMetadataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicMetadataServiceApplicationTests extends AbstractIntegrationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
